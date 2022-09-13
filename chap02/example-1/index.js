0c3ed487e77ca2333e62f9203f97c36d280ffe1a
Jaesung Ryu <cwryu6252@naver.com>
2022-09-13 21:28:08 +0900

    ex1

---
+++ chap02/example-1/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("hello world!");
+});
+
+app.listen(port, () => {
+    console.log(`First example app listening on port ${port}, point your browser at http://localhost:3000`);
+})
