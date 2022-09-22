a5f8d8562f7ab3fb408d9de69e1686f1f9b161ce
Jaesung Ryu <kade@afidev.com>
2022-09-22 10:05:06 +0900

    5-3 진행중

--- chap05/example/history/src/index.js
+++ chap05/example/history/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongodb = require('mongodb');
 const bodyParser = require('body-parser');
+const amqp = require('amqplib');
 
 if (!process.env.DBHOST) {
     throw new Error("Please specify the database host using environment variable DBHOST");
@@ -10,8 +11,13 @@ if (!process.env.DBNAME) {
     throw new Error("Please specify the database name using environment variable DBNAME");
 }
 
+if (!process.env.RABBIT) {
+    throw new Error("Please specify the RabbitMQ host using environment variable RABBIT");
+}
+
 const DBHOST = process.env.DBHOST;
 const DBNAME = process.env.DBNAME;
+const RABBIT = process.env.RABBIT;
 
 function connectDb() {
     return mongodb.MongoClient.connect(DBHOST)
@@ -20,45 +26,44 @@ function connectDb() {
         });
 }
 
-function setupHandlers(app, db) {
+function connectRabbit() {
+    console.log(`Connecting to RabbitMQ at ${RABBIT}`);
+
+    return amqp.connect(RABBIT)
+        .then(messagingConnection => {
+            console.log("Connected to RabbitMQ");
+            return messagingConnection.createChannel();
+        });
+}
+
+function setupHandlers(app, db, messageChannel) {
     const videoCollection = db.collection("videos");
 
-    app.post("/viewed", (req, res) => {
-        const videoPath = req.body.videoPath;
-        videoCollection.insertOne({ videoPath: videoPath })
-            .then(() => {
-                console.log(`Added video ${videoPath} to history`);
-                res.sendStatus(200);
-            })
-            .catch(err => {
-                console.error(`Error adding video ${videoPath} to history: ${err}`);
-                res.sendStatus(500);
-            });
-    });
+    function consumeViewedMessage(msg) {
+        console.log("Received a 'viewed' message");
 
-    app.get("/history", (req, res) => {
-        const skip = parseInt(req.query.skip);
-        const limit = parseInt(req.query.limit);
-        videoCollection.find()
-            .skip(skip)
-            .limit(limit)
-            .toArray()
-            .then(documents => {
-                res.json({ history: documents });
-            })
-            .catch(err => {
-                console.error(`Error retrieving history from database.`);
-                console.error(err && err.stack || err);
-                res.sendStatus(500);
+        const parsedMsg = JSON.parse(msg.content.toString());
+
+        return videoCollection.insertOne({ videoPath: parsedMsg.videoPath })
+            .then(() => {
+                console.log("Acknowledging message was handled");
+                messageChannel.ack(msg);
             });
-    });
+    }
+
+    return messageChannel.assertQueue("viewed", {})
+        .then(() => {
+            console.log("Asserted that the 'viewed' queue exists");
+            return messageChannel.consume("viewed", consumeViewedMessage);
+        })
 }
+
 
 function startHttpServer(db) {
     return new Promise(resolve => {
         const app = express();
         app.use(bodyParser.json());
-        setupHandlers(app, db);
+        setupHandlers(app, db, messageChannel);
 
         const port = process.env.PORT && parseInt(process.env.PORT) || 3000;
         app.listen(port, () => {
@@ -68,10 +73,15 @@ function startHttpServer(db) {
 }
 
 function main() {
-    return connectDb(DBHOST)
+    console.log("Hello World!");
+
+    return connectDb()
         .then(db => {
-            return startHttpServer(db);
-        })
+            return connectRabbit()
+                .then(messageChannel => {
+                    return startHttpServer(db, messageChannel);
+                });
+        });
 }
 
 main()
